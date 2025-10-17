b74bf8aea8f234173e1505d97840224c51623599
hyuli0520 <minhyeogjeon945@gmail.com>
2025-10-17 11:03:10 +0900

    feat: add protobuf struct & enum & message

--- MainServer/cpp/ServerPacketHandler.cpp
+++ MainServer/cpp/ServerPacketHandler.cpp
@@ -11,3 +11,18 @@ bool Handle_REQ_ENTER(Session* session, Protocol::REQ_ENTER& pkt)
 {
 	return false;
 }
+
+bool Handle_REQ_MOVE(Session* session, Protocol::REQ_MOVE& pkt)
+{
+	return false;
+}
+
+bool Handle_REQ_SAVE_INVENTORY(Session* session, Protocol::REQ_SAVE_INVENTORY& pkt)
+{
+	return false;
+}
+
+bool Handle_REQ_START_FISHING(Session* session, Protocol::REQ_START_FISHING& pkt)
+{
+	return false;
+}
--- MainServer/h/ServerPacketHandler.h
+++ MainServer/h/ServerPacketHandler.h
@@ -7,12 +7,22 @@ extern PacketHandlerFunc GPacketHandler[UINT16_MAX];
 enum : unsigned short
 {
 	PKT_REQ_ENTER = 1000,
-	PKT_RES_SPAWN = 1001,
-	PKT_NOTIFY_SPAWN = 1002,
+	PKT_RES_ENTER = 1001,
+	PKT_REQ_MOVE = 1002,
+	PKT_RES_MOVE = 1003,
+	PKT_REQ_SAVE_INVENTORY = 1004,
+	PKT_RES_SAVE_INVENTORY = 1005,
+	PKT_REQ_START_FISHING = 1006,
+	PKT_RES_BITE_BAIT = 1007,
+	PKT_RES_SPAWN = 1008,
+	PKT_NOTIFY_SPAWN = 1009,
 };
 
 bool Handle_INVALID(Session* session, BYTE* buffer, int len);
 bool Handle_REQ_ENTER(Session* session, Protocol::REQ_ENTER& pkt);
+bool Handle_REQ_MOVE(Session* session, Protocol::REQ_MOVE& pkt);
+bool Handle_REQ_SAVE_INVENTORY(Session* session, Protocol::REQ_SAVE_INVENTORY& pkt);
+bool Handle_REQ_START_FISHING(Session* session, Protocol::REQ_START_FISHING& pkt);
 
 class ServerPacketHandler
 {
@@ -22,7 +32,14 @@ public:
 		for (int i = 0; i < UINT16_MAX; i++)
 			GPacketHandler[i] = Handle_INVALID;
 		GPacketHandler[PKT_REQ_ENTER] = [](Session* session, BYTE* buffer, int len) {return HandlePacket<Protocol::REQ_ENTER>(Handle_REQ_ENTER, session, buffer, len); };
+		GPacketHandler[PKT_REQ_MOVE] = [](Session* session, BYTE* buffer, int len) {return HandlePacket<Protocol::REQ_MOVE>(Handle_REQ_MOVE, session, buffer, len); };
+		GPacketHandler[PKT_REQ_SAVE_INVENTORY] = [](Session* session, BYTE* buffer, int len) {return HandlePacket<Protocol::REQ_SAVE_INVENTORY>(Handle_REQ_SAVE_INVENTORY, session, buffer, len); };
+		GPacketHandler[PKT_REQ_START_FISHING] = [](Session* session, BYTE* buffer, int len) {return HandlePacket<Protocol::REQ_START_FISHING>(Handle_REQ_START_FISHING, session, buffer, len); };
 	}
+	static shared_ptr<vector<char>> MakeSendBuffer(Protocol::RES_ENTER& pkt) { return MakeSendBuffer(pkt, PKT_RES_ENTER); }
+	static shared_ptr<vector<char>> MakeSendBuffer(Protocol::RES_MOVE& pkt) { return MakeSendBuffer(pkt, PKT_RES_MOVE); }
+	static shared_ptr<vector<char>> MakeSendBuffer(Protocol::RES_SAVE_INVENTORY& pkt) { return MakeSendBuffer(pkt, PKT_RES_SAVE_INVENTORY); }
+	static shared_ptr<vector<char>> MakeSendBuffer(Protocol::RES_BITE_BAIT& pkt) { return MakeSendBuffer(pkt, PKT_RES_BITE_BAIT); }
 	static shared_ptr<vector<char>> MakeSendBuffer(Protocol::RES_SPAWN& pkt) { return MakeSendBuffer(pkt, PKT_RES_SPAWN); }
 	static shared_ptr<vector<char>> MakeSendBuffer(Protocol::NOTIFY_SPAWN& pkt) { return MakeSendBuffer(pkt, PKT_NOTIFY_SPAWN); }
 
