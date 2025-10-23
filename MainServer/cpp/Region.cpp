650e57d9f8f5e478a1f6e7921997781586e059ff
hyuli0520 <minhyeogjeon945@gmail.com>
2025-10-23 18:18:14 +0900

    feat: add player in region
    GameObject -> Player

--- MainServer/cpp/Region.cpp
+++ MainServer/cpp/Region.cpp
@@ -5,6 +5,25 @@
 Region::~Region()
 {
 }
+
+bool Region::AddObject(shared_ptr<GameObject> object)
+{
+	Protocol::ObjectType type = object->GetType();
+	if (type == Protocol::PLAYER)
+	{
+		if (m_players.find(object->GetId()) != m_players.end())
+			return false;
+
+		m_players.insert({ object->GetId(), static_pointer_cast<Player>(object) });
+	}
+
+	return true;
+}
+
+bool Region::RemoveObject(shared_ptr<GameObject> object)
+{
+}
+
 void Region::Tick()
 {
 	for (auto& obj : m_players)
--- MainServer/h/Region.h
+++ MainServer/h/Region.h
@@ -8,12 +8,15 @@ public:
 
 	int GetX() const { return m_x; }
 	int GetY() const { return m_y; }
-	unordered_map<unsigned long long, shared_ptr<GameObject>> GetObjects() const { return m_objects; }
+	unordered_map<unsigned long long, shared_ptr<Player>> GetPlayers() const { return m_players; }
+
+	bool AddObject(shared_ptr<GameObject> object);
+	bool RemoveObject(shared_ptr<GameObject> object);
 
 	void Tick();
 
 private:
 	int m_x;
 	int m_y;
-	unordered_map<unsigned long long, shared_ptr<GameObject>> m_objects;
+	unordered_map<unsigned long long, shared_ptr<Player>> m_players;
 };
