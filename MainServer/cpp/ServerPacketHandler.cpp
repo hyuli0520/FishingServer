879d2f6b92e8c638e3c2a142d3c50f4eaa879d8f
hyuli0520 <minhyeogjeon945@gmail.com>
2025-10-27 23:24:38 +0900

    fix: constructor & function error

--- MainServer/cpp/ServerPacketHandler.cpp
+++ MainServer/cpp/ServerPacketHandler.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "ServerPacketHandler.h"
 
+PacketHandlerFunc GPacketHandler[UINT16_MAX];
+
 bool Handle_INVALID(Session* session, BYTE* buffer, int len)
 {
 	PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
