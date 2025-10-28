9be7668fe1f8efe257d02f4b1bf1f508a42ea8c6
hyuli0520 <minhyeogjeon945@gmail.com>
2025-10-28 14:03:36 +0900

    feat: add getter & setter & create map

--- MainServer/h/MapManager.h
+++ MainServer/h/MapManager.h
@@ -1,8 +1,20 @@
 #pragma once
+using MapId = unsigned long long;
 
+class Map;
 class MapManager
 {
 public:
 	MapManager();
 	virtual ~MapManager();
+
+	shared_ptr<Map> CreateMap(MapId id, int w, int h, int size);
+	shared_ptr<Map> GetMap(MapId id) const;
+
+	void SetMain(MapId id);
+	shared_ptr<Map> Main() const { GetMap(m_mainId); }
+
+private:
+	concurrent_unordered_map<MapId, shared_ptr<Map>> m_maps;
+	MapId m_mainId = 1;
 };
