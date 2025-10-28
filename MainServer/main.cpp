736668034c6830a85f083e87953e9e75edcb3c7e
hyuli0520 <minhyeogjeon945@gmail.com>
2025-10-28 14:40:32 +0900

    feat: make main

--- MainServer/main.cpp
+++ MainServer/main.cpp
@@ -1,6 +1,27 @@
 #include "pch.h"
+#include "Thread/ThreadManager.h"
+#include "Network/Server.h"
 
 int main()
 {
+	ServerPacketHandler::Init();
+	cppx::native::init(5);
+	GEngine = new Engine();
+	GManager = new Manager();
 
+	GEngine->Init();
+	GManager->Init();
+
+	auto endpoint = cppx::endpoint(cppx::ip_address::any, 3333);
+	auto server = Server::Make<GameSession>();
+	server->Run(endpoint);
+
+	GEngine->RunThread(4);
+
+	while (true)
+	{
+		this_thread::sleep_for(0.1s);
+	}
+
+	GEngine->GetThreadManager()->Join();
 }
